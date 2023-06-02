9c01461f2706ee55b61616fefbff259d6db60af7
Hussein <husseinhijazii10@gmail.com>
2023-06-02 12:55:43 +0300

    Initial Commit

---
+++ uhuUITests/uhuUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  uhuUITestsLaunchTests.swift
+//  uhuUITests
+//
+//  Created by Hussein Hijazzi on 02/06/2023.
+//
+
+import XCTest
+
+final class uhuUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
